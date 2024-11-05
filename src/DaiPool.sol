5895842af112142c6eb3626cfe8b2759ff08a7b3
fabiandiaz416 <diazfabian99@icloud.com>
2024-11-05 15:37:58 -0300

    add functions into DaiPool

--- src/DaiPool.sol
+++ src/DaiPool.sol
@@ -1,20 +1,90 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.27;
 
-import { Ownable } from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
+contract DaiPool is Ownable {
+    using SafeERC20 for IERC20;
 
-Error NoZeroValue(uint256 amount);
-    
-contract DaiPool {
+    error InvalidAmount(uint256 amount);
+    error NoFundsToWithdraw(uint256 amount);
 
-    constructor(address initialAdmin) {
-        transferOwnership(initialAdmin);
+    struct User {
+        uint256 depositAmount;
+        uint256 rewardDebt;
+        uint256 lastRewardsSnapshot;
+    }
 
-    }   
+    using SafeERC20 for IERC20;
+    IERC20 public rewardToken;
+    uint256 public totalDeposits;
+    uint256 public totalRewards;
+    uint256 public lastRewardBlock;
 
-    function depositDai(uint256 _amount) public {
-        if (amount == 0) revert NoZeroValue(_amount);
+    mapping(address => User) userDeposits;
 
+    event Withdraw(address user, uint256 amountWithdrawed);
+    event Deposit(address user, uint256 amountDeposited);
+    event DepositRewards(uint256 amount);
+
+    constructor(
+        address initialAdmin,
+        address _rewardToken
+    ) Ownable(initialAdmin) {
+        rewardToken = IERC20(_rewardToken);
+    }
+
+    modifier updateRewards(address _user) {
+        User storage user = userDeposits[_user];
+        if (user.depositAmount > 0 && totalDeposits > 0) {
+            uint256 userRewards = (user.depositAmount *
+                (totalRewards - user.lastRewardsSnapshot)) / totalDeposits;
+            user.rewardDebt += userRewards;
+            user.lastRewardsSnapshot = totalRewards;
+        }
+        _;
+    }
+
+    function depositDai(uint256 _amount) public updateRewards(msg.sender) {
+        if (_amount < 1) revert InvalidAmount(_amount);
+        User storage user = userDeposits[msg.sender];
+        user.depositAmount += _amount;
+        user.lastRewardsSnapshot = totalRewards;
+
+        totalDeposits += _amount;
+
+        rewardToken.safeTransferFrom(msg.sender, address(this), _amount);
+
+        emit Deposit(msg.sender, _amount);
+    }
+
+    function claimRewardsAndDeposit(
+        uint256 _amount
+    ) public updateRewards(msg.sender) {
+        User storage user = userDeposits[msg.sender];
+        if (user.depositAmount < 1) revert NoFundsToWithdraw(_amount);
+
+        uint256 totalAmount = user.depositAmount + user.rewardDebt;
+
+        totalDeposits -= user.depositAmount;
+
+        // Reset the data
+        user.depositAmount = 0;
+        user.rewardDebt = 0;
+        user.lastRewardsSnapshot = 0;
+
+        // Interaction sending the funds and rewards to the user
+        rewardToken.safeTransfer(msg.sender, totalAmount);
+        emit Withdraw(msg.sender, totalAmount);
+    }
+
+    function depositRewards(uint256 _amount) public onlyOwner {
+        if (_amount < 1) revert InvalidAmount(_amount);
+        totalRewards += _amount;
+        rewardToken.safeTransfer(address(this), _amount);
+
+        emit DepositRewards(_amount);
     }
 }
